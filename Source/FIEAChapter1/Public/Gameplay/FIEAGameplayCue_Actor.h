62e0aa35b5285eb1888482f47502bdfd38afa1bb
Darksearcher <vic9050@gmail.com>
2024-02-25 22:28:24 -0600

    FootSteps and effects

---
+++ Source/FIEAChapter1/Public/Gameplay/FIEAGameplayCue_Actor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayCueNotify_Actor.h"
+#include "FIEAGameplayCue_Actor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIEACHAPTER1_API AFIEAGameplayCue_Actor : public AGameplayCueNotify_Actor
+{
+	GENERATED_BODY()
+	
+};
